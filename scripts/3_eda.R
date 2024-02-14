583ea242ba57c035fce4b8bca7362e25dcb2fddc
cassielee2025 <145501426+cassielee2025@users.noreply.github.com>
2024-02-14 16:55:47 -0600

    why doesnt my for loop work...

--- scripts/3_eda.R
+++ scripts/3_eda.R
@@ -9,3 +9,48 @@ library(here)
 tidymodels_prefer()
 
 # load EDA data
+load(here("data/data_split/birth_eda.rda"))
+
+# skim data
+birth_eda %>% 
+  skimr::skim_without_charts()
+
+# factor variables ----
+
+# function for plotting factor variables
+plot_factor <- function(var){
+  
+  # create boxplot between factor variable and birthweight
+  birth_eda %>% 
+    ggplot(aes({{var}}, dbwt)) +
+    geom_boxplot() +
+    theme_classic()
+  
+  # create file name
+  file_name <- paste0("factor_", deparse(substitute(var)), ".png")
+  
+  # save file
+  ggsave(
+    file_name, 
+    plot = last_plot(), 
+    path = here("eda_output/")
+  )
+}
+
+# check that the function works
+plot_factor(attend)
+
+# get list of factor variables
+factor_vars <- birth_eda %>% 
+  select_if(is.factor) %>% 
+  names()
+
+# plot for all factor variables
+for(i in factor_vars){
+  
+  # get raw variable name
+  var_name <- as.name(i)
+  
+  # make and save plot
+  plot_factor(var_name)
+}
