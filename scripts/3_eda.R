1bf0b135c04ecb2acdd2ce527f0d4f3b2deaa3d6
cassielee2025 <145501426+cassielee2025@users.noreply.github.com>
2024-02-14 17:10:22 -0600

    plot_factor(var_name) doesn't work but plot_factor(attend) does?

--- scripts/3_eda.R
+++ scripts/3_eda.R
@@ -39,6 +39,7 @@ plot_factor <- function(var){
 
 # check that the function works
 plot_factor(attend)
+plot_factor(bfacil)
 
 # get list of factor variables
 factor_vars <- birth_eda %>% 
@@ -54,3 +55,7 @@ for(i in factor_vars){
   # make and save plot
   plot_factor(var_name)
 }
+
+var_name <- as.name("attend")
+plot_factor(var_name) # why doesn't this work
+plot_factor(attend)
