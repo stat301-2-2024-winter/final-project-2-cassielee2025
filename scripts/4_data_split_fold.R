077cfc39fb475b69ba22b01b64e4af8aa4956b1f
cassielee2025 <145501426+cassielee2025@users.noreply.github.com>
2024-02-15 12:04:22 -0600

    change number of folds

--- scripts/4_data_split_fold.R
+++ scripts/4_data_split_fold.R
@@ -33,16 +33,16 @@ birth_test <- birth_split %>%
 # how many folds and repeats?
 # training set has 30,000 observations
 # if i do 10 folds, 27,0000 observations for training 3,000 for getting metrics
-# if i do 10 folds and 5 repeats, that is 50 models
-# it takes my computer 1.25 minutes to run 50 rf models with 300 training observations
-# so it should take 125 minutes to run 50 rf models with 30,000 training observations
+# if i do 5 folds and 5 repeats, that is 25 models
+# it takes my computer 1.25 minutes to run 50 rf models with 300 training observations and 14 predictors
+# so it should take about 225 (3.75) to run 25 rf models with 30,000 training observations
 # that should be ok???
 
 # set seed
 set.seed(9813274)
 
 birth_fold <- birth_train %>% 
-  vfold_cv(v = 10, repeats = 5, strata = dbwt)
+  vfold_cv(v = 5, repeats = 5, strata = dbwt)
 
 # save data splits 
 save(birth_train, file = here("data/data_split/birth_train.rda"))
