2fdce2e79456f4aea7b6228fbee43c21e29e113b
cassielee2025 <145501426+cassielee2025@users.noreply.github.com>
2024-02-22 17:22:31 -0600

    new recipes

--- scripts/5_recipes.R
+++ scripts/5_recipes.R
@@ -14,14 +14,21 @@ load(here("data/data_split/birth_train.rda"))
 ################################################################################
 # basic recipes using variables as is ----
 ################################################################################
-# recipe for linear and elastic net
+# recipe for linear, elastic net, and neural network models
 birth_rec1a <- birth_train %>% 
   recipe(dbwt ~ .) %>% 
-  # change NA in interval (numeric) variables to 0 if plural delivery
-  # and change NA in precare to 10 (essentially negative amounts of prenatal care) if no precare
-  step_mutate(across(c(illb_r, ilop_r, ilp_r), \(x) if_else(is.na(x), 0, x)),
-              precare = if_else(is.na(precare), 10, precare)) %>% 
-  step_mutate(across(c(plural_del, any_precare), as.character)) %>% 
+  step_mutate(
+    # change NA in interval (numeric) variables to 0 if plural delivery
+    across(c(illb_r, ilop_r, ilp_r), \(x) if_else(is.na(x), 0, x)),
+    
+    # and change NA in precare to 10 (essentially negative amounts of prenatal care) if no precare
+    precare = if_else(is.na(precare), 10, precare),
+    
+    # change logical data type to character
+    across(c(plural_del, any_precare), as.character)
+  ) %>% 
+  
+  #change string to factor
   step_string2factor(plural_del, any_precare) %>% 
   step_unknown(all_nominal_predictors()) %>% 
   step_dummy(all_nominal_predictors()) %>% 
@@ -38,17 +45,22 @@ birth_rec1a %>%
 # recipe for tree based models
 birth_rec1b <- birth_train %>% 
   recipe(dbwt ~ .) %>% 
-  # change NA in interval (numeric) variables to 0 if plural delivery
-  # and change NA in precare to 10 (essentially negative amounts of prenatal care) if no precare
-  step_mutate(across(c(illb_r, ilop_r, ilp_r), \(x) if_else(is.na(x), 0, x)),
-              precare = if_else(is.na(precare), 10, precare)) %>% 
-  step_mutate(across(c(plural_del, any_precare), as.character)) %>% 
+  step_mutate(
+    # change NA in interval (numeric) variables to 0 if plural delivery
+    across(c(illb_r, ilop_r, ilp_r), \(x) if_else(is.na(x), 0, x)),
+    
+    # and change NA in precare to 10 (essentially negative amounts of prenatal care) if no precare
+    precare = if_else(is.na(precare), 10, precare),
+    
+    # change logical data type to character
+    across(c(plural_del, any_precare), as.character)
+  ) %>% 
+  #change string to factor
   step_string2factor(plural_del, any_precare) %>% 
   step_unknown(all_nominal_predictors()) %>% 
   step_dummy(all_nominal_predictors(), one_hot = TRUE) %>% 
   step_zv(all_predictors()) %>% 
   step_lincomb(all_predictors())
-
 
 birth_rec1b %>% 
   prep() %>% 
@@ -57,9 +69,89 @@ birth_rec1b %>%
 
 # save recipes 
 save(birth_rec1a, file = here("recipes/birth_rec1a.rda"))
-save(birth_rec1b, file = here("recipes/birth_recb.rda"))
+save(birth_rec1b, file = here("recipes/birth_rec1b.rda"))
 
 ################################################################################
-# recipe using binary variables ----
+# recipe using prenatal in first trimester care as a binary variable and interaction terms ----
 ################################################################################
+# recipe for linear, elastic net, and neural network models
+birth_rec2a <- birth_train %>% 
+  recipe(dbwt ~ .) %>% 
+  step_mutate(
+    # change NA in interval (numeric) variables to 0 if plural delivery
+    across(c(illb_r, ilop_r, ilp_r), \(x) if_else(is.na(x), 0, x)),
+    
+    # was there prenatal care beginning in the first trimester?
+    first_tri_precare = case_when(
+      is.na(precare) ~ FALSE,
+      precare <= 3 ~ TRUE,
+      precare > 3 ~ FALSE
+    ),
+    
+    # change logical data type to character
+    across(c(first_tri_precare, plural_del, any_precare), as.character)
+  ) %>% 
+  # remove bmi because likely correlation with pre-pregnancy weight and height
+  # and pay because pay and pay_recode are similar
+  step_rm(precare, bmi, pay) %>% 
+  step_string2factor(first_tri_precare, plural_del, any_precare) %>% 
+  step_unknown(all_nominal_predictors()) %>% 
+  step_dummy(all_nominal_predictors()) %>% 
+  step_zv(all_predictors()) %>% 
+  step_lincomb(all_predictors()) %>% 
+  step_interact(
+    # interaction between starting prenatal care early and number of prenatal visits
+    ~ starts_with("first_tri_precare"):previs,
+  ) %>% 
+  step_interact(
+    # interaction between weight gain and pre-pregnancy weight
+    ~ p_wgt_r:wtgain
+  ) %>% 
+  step_scale(all_numeric_predictors()) %>% 
+  step_center(all_numeric_predictors())
 
+birth_rec2a %>% 
+  prep() %>% 
+  bake(new_data = NULL)
+
+# recipe for tree based models
+birth_rec2b <- birth_train %>% 
+  recipe(dbwt ~ .) %>% 
+  step_mutate(
+    # change NA in interval (numeric) variables to 0 if plural delivery
+    across(c(illb_r, ilop_r, ilp_r), \(x) if_else(is.na(x), 0, x)),
+    
+    # was there prenatal care beginning in the first trimester?
+    first_tri_precare = case_when(
+      is.na(precare) ~ FALSE,
+      precare <= 3 ~ TRUE,
+      precare > 3 ~ FALSE
+    ),
+    
+    # change logical data type to character
+    across(c(first_tri_precare, plural_del, any_precare), as.character)
+  ) %>% 
+  # remove bmi because likely correlation with pre-pregnancy weight and height
+  # and pay because pay and pay_recode are similar
+  step_rm(precare, bmi, pay) %>% 
+  step_string2factor(first_tri_precare, plural_del, any_precare) %>% 
+  step_unknown(all_nominal_predictors()) %>% 
+  step_dummy(all_nominal_predictors(), one_hot = TRUE) %>% 
+  step_zv(all_predictors()) %>% 
+  step_lincomb(all_predictors()) %>% 
+  step_interact(
+    # interaction between starting prenatal care early and number of prenatal visits
+    ~ starts_with("first_tri_precare"):previs,
+  ) %>% 
+  step_interact(
+    # interaction between weight gain and pre-pregnancy weight
+    ~ p_wgt_r:wtgain
+  )
+
+birth_rec2b %>% 
+  prep() %>% 
+  bake(new_data = NULL)
+
+# save recipes 
+save(birth_rec2a, file = here("recipes/birth_rec2a.rda"))
+save(birth_rec2b, file = here("recipes/birth_rec2b.rda"))
