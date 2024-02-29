984e9929dd3fc2a651a7721a6f588dcfe2ca1b8e
cassielee2025 <145501426+cassielee2025@users.noreply.github.com>
2024-02-29 10:57:42 -0600

    collect all metrics

--- scripts/7_model_analysis.R
+++ scripts/7_model_analysis.R
@@ -13,6 +13,12 @@ load(here("results/nnet_tuned.rda"))
 load(here("results/null_fit.rda"))
 load(here("results/rf_tuned.rda"))
 
+load(here("results/bt_tuned2.rda"))
+load(here("results/en_tuned2.rda"))
+load(here("results/lm_fit2.rda"))
+load(here("results/nnet_tuned2.rda"))
+load(here("results/rf_tuned2.rda"))
+
 # null and lm fit metrics ----
 null_lm_metrics <- bind_rows(
   
@@ -21,6 +27,10 @@ null_lm_metrics <- bind_rows(
     filter(.metric == "rmse"),
   
   "lm" = lm_fit %>% 
+    collect_metrics() %>% 
+    filter(.metric == "rmse"),
+  
+  "lm2" = lm_fit2 %>% 
     collect_metrics() %>% 
     filter(.metric == "rmse"),
   
@@ -33,7 +43,11 @@ all_fits <- as_workflow_set(
   bt = bt_tuned,
   en = en_tuned,
   nnet = nnet_tuned,
-  rf = rf_tuned
+  rf = rf_tuned,
+  bt2 = bt_tuned2,
+  en2 = en_tuned2,
+  nnet2 = nnet_tuned2,
+  rf2 = rf_tuned2
 )
 
 all_metrics <- all_fits %>% 
