a0bd9a46b6a728aeff6a022eb07c6aae76bf9df8
cassielee2025 <145501426+cassielee2025@users.noreply.github.com>
2024-03-03 21:40:40 -0600

    change alpha of points

---
+++ scripts/11_analyze_final_model.R
@@ -0,0 +1,36 @@
+# Final model performance
+
+# load packages ----
+library(tidyverse)
+library(tidymodels)
+library(here)
+
+# handle common conflicts
+tidymodels_prefer()
+
+# load testing data
+load(here("data/data_split/birth_test.rda"))
+
+# load fit
+load(here("results/final_fit.rda"))
+
+# set metrics
+bt_metrics <- metric_set(rmse, rsq, mae)
+
+# predictions with testing set
+birth_prediction <- birth_test %>% 
+  select(dbwt) %>% 
+  bind_cols(predict(final_fit, birth_test))
+
+# get metrics
+test_metrics <- bt_metrics(birth_prediction, truth = dbwt, estimate = .pred)
+save(test_metrics, file = here("results/test_metrics.rda"))
+
+# visualize results
+ggplot(birth_prediction, aes(x = dbwt, y = .pred)) + 
+  geom_abline(lty = 2) + 
+  geom_point(alpha = 0.3) + 
+  labs(y = "Predicted Birth Weight", x = "Actual Birth Weight") +
+  coord_obs_pred()
+
+ggsave(plot = last_plot(), filename = "predictions_plot.png", path = here("results/"))
